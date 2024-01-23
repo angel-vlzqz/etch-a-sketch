a8683b065f7309e664f65e182fc48bfb221f64ba
angel-vlzqz <106009767+angel-vlzqz@users.noreply.github.com>
2024-01-23 16:38:38 -0700

    add functionality: TODO: allow slider to determine grid size

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.getElementById('sketch');
+
+    // TODO: Add the slider functionality
+    const dimension = document.getElementById('slider')
+    for (let i = 0; i < 256; i++) { // 16x16 grid
+        const gridElement = document.createElement('div');
+        gridElement.addEventListener('mouseover', draw); // Add drawing functionality
+        grid.appendChild(gridElement);
+    }
+
+    // Function to draw (change color of the div)
+    function draw(e) {
+        e.target.style.backgroundColor = 'black'; // Change the color to black
+    }
+
+    // Shake to clear
+    document.getElementById('clear').addEventListener('click', () => {
+        grid.querySelectorAll('div').forEach(div => {
+            div.style.backgroundColor = '';
+        });
+    });
+});
